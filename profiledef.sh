0b2cae872cb8543ab7b515808e76a11ff0d54f55
smtdfc <smtdfca@gmail.com>
2026-06-13 09:23:35 +0700

    feat: Add firewall

--- profiledef.sh
+++ profiledef.sh
@@ -18,6 +18,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
+
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
@@ -31,7 +32,7 @@ file_permissions=(
 
   ["/etc/issue"]="0:0:644"
   ["/etc/os-release"]="0:0:644"
-
+  ["/etc/nftables.conf"]="0:0:644"
 
 
   ["/usr/share/bakery/logo.txt"]="0:0:644"
